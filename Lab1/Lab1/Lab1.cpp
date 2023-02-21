c47f5609d00000bb33dbb1936ac8dcbd2f25afae
Gheorghita MUTU <gmutu@bitdefender.biz>
2023-02-21 08:53:52 +0200

    + Lab 1 - ex 1

--- Lab1/Lab1/Lab1.cpp
+++ Lab1/Lab1/Lab1.cpp
@@ -1,20 +1,44 @@
-// Lab1.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#pragma warning(disable:4996)
+
+int MyAtoi(char* str)
+{
+    int result = 0;
+    for (int i = 0; str[i] <= '9' && str[i] >= '0'; ++i)
+    {
+        result = result * 10 + str[i] - '0';
+    }
+
+    return result;
+}
+
+void ex_1()
+{
+    FILE* fp = fopen("in.txt", "r");
+    if (!fp)
+    {
+        perror("File opening failed");
+        return;
+    }
+
+    int sum = 0;
+    char line[1024]{ 0 };
+    while (fgets(line, 1024, fp))
+    {
+        sum += MyAtoi(line);
+    }
+
+    printf("Ex 1: Sum is <%d>.\n", sum);
+}
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    ex_1();
+
+    return 0;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
